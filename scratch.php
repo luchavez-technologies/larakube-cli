3f459a972b4667e17c0f0e0b55c72caba959fc2c
James Carlo Luchavez <jamescarloluchavez@gmail.com>
2026-08-10 09:39:56 +0800

    chore: Deleted temp test files

--- scratch.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require __DIR__.'/vendor/autoload.php';
-$app = require_once __DIR__.'/bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-echo view('k8s.traefik-managed', ['email' => 'james@luchtech.dev', 'loadBalancerName' => 'larakube-do-sgp1-luchtech-managed'])->render();
